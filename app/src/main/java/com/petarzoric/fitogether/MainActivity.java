a1e38990ee6db0c7c86075a86c2325a86156ce58
Petar Zoric <petar.zoric@web.de>
2017-11-20 18:44:18 +0100

    Initial commit

---
+++ app/src/main/java/com/petarzoric/fitogether/MainActivity.java
@@ -0,0 +1,19 @@
+package com.petarzoric.fitogether;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void testUpload(){
+
+        int i = 2;
+    }
+}
