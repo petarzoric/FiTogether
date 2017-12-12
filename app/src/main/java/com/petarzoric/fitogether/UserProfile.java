85c7a5fd75d2a9543c0a7663a1b18abd9a31e466
Petar Zoric <petar.zoric@web.de>
2017-12-12 22:55:11 +0100

    Userprofile mit image url und bisschen refactored

--- app/src/main/java/com/petarzoric/fitogether/UserProfile.java
+++ app/src/main/java/com/petarzoric/fitogether/UserProfile.java
@@ -12,8 +12,10 @@ public class UserProfile {
     int studio;
     int location;
     int gender;
+    String imageURL;
+    String thumbURL;
 
-    public UserProfile(String email,String name,int age,   int userlevel, int studio, int location, int gender) {
+    public UserProfile(String email,String name,int age,   int userlevel, int studio, int location, int gender, String iurl, String turl ) {
         this.email = email;
         this.name = name;
         this.age = age;
@@ -21,10 +23,23 @@ public class UserProfile {
         this.studio = studio;
         this.location = location;
         this.gender = gender;
+        this.imageURL = iurl;
+        this.thumbURL = turl;
     }
 
-    public UserProfile() {
+    public UserProfile(){
+        this.email = "default";
+        this.name = "default";
+        this.age = -1;
+        this.userlevel = -1;
+        this.location = -1;
+        this.gender = -1;
+        this.imageURL = "default";
+        this.thumbURL = "default";
+
     }
+
+
     public String getEmail() {
         return email;
     }
@@ -48,36 +63,58 @@ public class UserProfile {
         this.age = age;
     }
 
-
+    //GETTER
 
     public int getUserlevel() {
         return userlevel;
     }
 
-    public void setUserlevel(int userlevel) {
-        this.userlevel = userlevel;
-    }
     public int getStudio() {
         return studio;
-    }
-
-    public void setStudio(int studio) {
-        this.studio = studio;
     }
 
     public int getLocation() {
         return location;
     }
 
-    public void setLocation(int location) {
-        this.location = location;
-    }
-
     public int getGender() {
         return gender;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public String getThumbURL() {
+        return thumbURL;
+    }
+
+    //SETTER
+
+
+    public void setUserlevel(int userlevel) {
+        this.userlevel = userlevel;
+    }
+
+    public void setStudio(int studio) {
+        this.studio = studio;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
     }
 
     public void setGender(int gender) {
         this.gender = gender;
     }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public void setThumbURL(String thumbURL) {
+        this.thumbURL = thumbURL;
+    }
 }
+
+
---
+++ app/src/main/java/userProfileConverter.java
@@ -0,0 +1,6 @@
+/**
+ * Created by petarzoric on 12.12.17.
+ */
+
+public class userProfileConverter {
+}
