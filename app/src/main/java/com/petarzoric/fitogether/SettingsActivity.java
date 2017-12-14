049ae27ff52ecba843ccda0b43afb788ec401798
Petar Zoric <petar.zoric@web.de>
2017-12-14 02:37:19 +0100

    Bilder können nun hochgeladen werden, gg

--- app/src/main/java/com/petarzoric/fitogether/SettingsActivity.java
+++ app/src/main/java/com/petarzoric/fitogether/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.petarzoric.fitogether;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -15,6 +19,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+import java.util.UUID;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -24,6 +34,9 @@ public class SettingsActivity extends AppCompatActivity {
     private Button changeImageButton;
     private TextView displayName;
     private TextView displayStatus;
+    private static final int GALLERY_PICK = 1;
+
+    private StorageReference imageStorage;
 
 
 
@@ -37,6 +50,7 @@ public class SettingsActivity extends AppCompatActivity {
         displayName = (TextView) findViewById(R.id.displayName);
         displayStatus = (TextView) findViewById(R.id.hiThere);
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        imageStorage = FirebaseStorage.getInstance().getReference();
 
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
         String current_uid = currentUser.getUid();
@@ -67,5 +81,75 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        changeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /*
+                Intent galleryIntent = new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(galleryIntent, "SELECT IMAGE"), GALLERY_PICK);
+                */
+
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(SettingsActivity.this);
+
+
+
+
+
+
+            }
+        });
+
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_PICK && requestCode == RESULT_OK){
+
+            Uri imageUrl = data.getData();
+
+            CropImage.activity(imageUrl)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+
+            //Toast.makeText(SettingsActivity.this, imageUrl, Toast.LENGTH_LONG).show();
+
+
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+
+                Uri resultUri = result.getUri();
+
+                StorageReference  filepath = imageStorage.child("profile_images").child(UUID.randomUUID().toString().substring(0,8));
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SettingsActivity.this, "SUCCESSFUL", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            Toast.makeText(SettingsActivity.this, "error in uploading", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+
+
 }
