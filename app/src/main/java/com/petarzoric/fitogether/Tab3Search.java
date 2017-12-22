3627473caac18a3029766aad5ab81e3006c4381c
Alex V <a.vahid@campus.lmu.de>
2017-12-22 15:42:17 +0100

    search angefagen anzupassen

--- app/src/main/java/com/petarzoric/fitogether/Tab3Search.java
+++ app/src/main/java/com/petarzoric/fitogether/Tab3Search.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * Created by Alex on 24.11.2017.
@@ -14,16 +16,36 @@ import android.widget.Button;
 
 public class Tab3Search extends Fragment {
     Button search;
+    int level;
+    int gender;
+    int muscle;
+    Spinner levelspinner;
+    Spinner genderspinner;
+    Spinner musclespinner;
 
     public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab3search, container, false);
         search = rootView.findViewById(R.id.searchbutton);
+        levelspinner = rootView.findViewById(R.id.levelspinner);
+        genderspinner = rootView.findViewById(R.id.genderspinner);
+        musclespinner = rootView.findViewById(R.id.musclespinner);
+        ArrayAdapter<CharSequence> trainingadapter = ArrayAdapter.createFromResource(getActivity(), R.array.Training, R.layout.support_simple_spinner_dropdown_item);
+        musclespinner.setAdapter(trainingadapter);
+        ArrayAdapter<CharSequence> genderadapter = ArrayAdapter.createFromResource(getActivity(), R.array.Geschlecht, R.layout.support_simple_spinner_dropdown_item);
+        genderspinner.setAdapter(genderadapter);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Level, R.layout.support_simple_spinner_dropdown_item);
+        levelspinner.setAdapter(adapter);
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                level = levelspinner.getSelectedItemPosition();
+                gender = genderspinner.getSelectedItemPosition();
+                muscle = musclespinner.getSelectedItemPosition();
                 Intent intent = new Intent(getActivity(), SearchResults.class);
-                intent.putExtra("level", 1); // dummylevel
+                intent.putExtra("level", level);
+                intent.putExtra("muscle", muscle);
+                intent.putExtra("gender", gender);
                 startActivity(intent);
             }
         });
