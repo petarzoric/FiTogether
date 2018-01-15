d4f868187210ab5a587f0ddc8525d67716cd9b63
Petar Zoric <petar.zoric@web.de>
2018-01-16 00:39:23 +0100

    Notifications bissl weiter gemacht, sollten eig intent zu dem jeweiligem Profil machen, tun sie aber noch nicht

--- app/src/main/java/com/petarzoric/fitogether/FirebaseMessagingService.java
+++ app/src/main/java/com/petarzoric/fitogether/FirebaseMessagingService.java
@@ -1,6 +1,8 @@
 package com.petarzoric.fitogether;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.RemoteMessage;
@@ -14,11 +16,36 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+
+        String notificationTitle = remoteMessage.getNotification().getTitle();
+        String notificationBody = remoteMessage.getNotification().getBody();
+
+        String click_action = remoteMessage.getNotification().getClickAction();
+
+        String fromUserID = remoteMessage.getData().get("from_user_id");
+
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_launcher_background)
-                        .setContentTitle("New Friend Request")
-                        .setContentText("You received a new friend request");
+                        .setContentTitle(notificationTitle)
+                        .setContentText(notificationBody);
+
+        Intent resultIntent = new Intent(click_action);
+        resultIntent.putExtra("user_id", fromUserID);
+
+
+
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        mBuilder.setContentIntent(resultPendingIntent);
+
+
 
         int mNotificationId = (int) System.currentTimeMillis();
         NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
