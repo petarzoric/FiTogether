98f1aa4073303e1428542a76558d58921ea4b8f2
Alex V <a.vahid@campus.lmu.de>
2018-01-26 15:55:43 +0100

    anpassen des CalenderTabs

---
+++ app/src/main/java/com/petarzoric/fitogether/DateResults.java
@@ -0,0 +1,183 @@
+package com.petarzoric.fitogether;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class DateResults extends AppCompatActivity {
+    ArrayList<UserTraining> matches = new ArrayList<>();
+    FirebaseDatabase database;
+    DatabaseReference databaseReference;
+    UserProfile[] userProfiles;
+    FirebaseAuth auth;
+    String currentUserId;
+    Dialog popupDialog;
+    Button requestButton;
+    TextView userName;
+    TextView userStudio;
+    EditText userMessage;
+    CircleImageView userImage;
+
+    RecyclerView recycleList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_results);
+        recycleList = findViewById(R.id.recyclelist);
+        recycleList.setHasFixedSize(true);
+        recycleList.setLayoutManager(new LinearLayoutManager(this));
+        auth = FirebaseAuth.getInstance();
+
+        currentUserId = auth.getCurrentUser().getUid();
+        popupDialog = new Dialog(this);
+
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference();
+        final Intent data = getIntent();
+        final String month = data.getStringExtra("month");
+        final String day = data.getStringExtra("day");
+
+        if ( databaseReference.child("TrainingsDate").child(month).child(day)!= null){
+            databaseReference.child("TrainingsDate").child(month).child(day).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                    for (DataSnapshot child : children) {
+                        if ((!child.getKey().equals(FirebaseAuth.getInstance().getCurrentUser().getUid()))) {
+                            matches.add(child.getValue(UserTraining.class));
+                        }
+
+                    }
+
+                    matchesToProfile(matches);
+
+                    FirebaseRecyclerAdapter<UserResults, SearchViewHolder> firebaseRecyclerAdapter= new FirebaseRecyclerAdapter<UserResults, SearchViewHolder>(
+                            UserResults.class, R.layout.listviewitems, SearchViewHolder.class, databaseReference.child("Searchresults").child(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+                        @Override
+                        protected void populateViewHolder(SearchViewHolder viewHolder, UserResults model, int position) {
+                            viewHolder.setName(model.getName());
+                            viewHolder.setAge(String.valueOf(model.getAge()));
+                            viewHolder.setLevel(Level.parseToString(model.getLevel()));
+                            viewHolder.setGender(Gender.parseToString(model.getGender()));
+                            viewHolder.setImage(model.getThumbnail(), getApplicationContext());
+                            viewHolder.setTime(model.getTime());
+                            viewHolder.setStudio(Converter.studioString(model.getStudio(), model.getLocation(),getResources()));
+
+                            viewHolder.view.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+
+                                    TextView closeIcon;
+                                    Button requestButton;
+
+                                    popupDialog.setContentView(R.layout.result_popup);
+                                    closeIcon = (TextView) popupDialog.findViewById(R.id.close);
+                                    requestButton = popupDialog.findViewById(R.id.popup_button);
+                                    closeIcon.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            popupDialog.dismiss();
+                                        }
+                                    });
+
+                                    requestButton.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            //TODO ANFRAGE SCHICKEN
+                                        }
+                                    });
+
+                                    popupDialog.show();
+                                }
+                            });
+
+                        }
+
+                    };
+
+                    recycleList.setAdapter(firebaseRecyclerAdapter);
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });}
+
+    }
+
+    public UserProfile[] matchesToProfile(final ArrayList<UserTraining> match) {
+        userProfiles = new UserProfile[match.size()];
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference();
+
+        String key = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        databaseReference.child("Searchresults").child(key).removeValue();
+        databaseReference.child("Users2").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ArrayList<UserProfile> profiles = new ArrayList<>();
+
+                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                int count = 0;
+                for (DataSnapshot child : children) {
+                    profiles.add(child.getValue(UserProfile.class));
+                    for (int i = 0; i < match.size(); i++) {
+                        if (child.getKey().equals(match.get(i).getUser()) ) {
+                            //  if (Level.parseToInt(child.getValue(UserProfile.class).getLevel()) == level) {
+                            userProfiles[count] = child.getValue(UserProfile.class);
+                            UserResults results = new UserResults(userProfiles[i].getUid(), userProfiles[i].getName(), userProfiles[i].getAge(), userProfiles[i].getLevel(), userProfiles[i].getStudio(), userProfiles[i].getLocation(), userProfiles[i].getGender(), userProfiles[i].getThumbnail(), match.get(i).getTime());
+                            databaseReference.child("Searchresults").child(key).child(child.getKey()).setValue(results);
+                            count++;
+                            //    }
+
+                        }
+                    }
+                }
+                if (count == 0){
+                    Toast.makeText(DateResults.this, "No Results found", Toast.LENGTH_SHORT).show();
+                    DateResults.super.onBackPressed();
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        return userProfiles;
+
+    }
+}
+
+
+
--- app/src/main/java/com/petarzoric/fitogether/Tab2Calender.java
+++ app/src/main/java/com/petarzoric/fitogether/Tab2Calender.java
@@ -1,6 +1,8 @@
 package com.petarzoric.fitogether;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TimePicker;
@@ -31,6 +34,7 @@ import java.util.TimeZone;
 public class Tab2Calender extends Fragment {
     Spinner training;
     Button saveTraining;
+    Button search;
     CalendarView calendar;
     String selectedDate;
     String key;
@@ -47,6 +51,8 @@ public class Tab2Calender extends Fragment {
     int d;
     EditText time;
     String times;
+    EditText dayView;
+    EditText monthView;
 
 
 
@@ -57,26 +63,57 @@ public class Tab2Calender extends Fragment {
         ArrayAdapter<CharSequence> trainingadapter = ArrayAdapter.createFromResource(getActivity(), R.array.Training, R.layout.support_simple_spinner_dropdown_item);
         training.setAdapter(trainingadapter);
         time = rootView.findViewById(R.id.timestart);
+        dayView = rootView.findViewById(R.id.dayView);
+        monthView = rootView.findViewById(R.id.monthView);
         saveTraining = rootView.findViewById(R.id.savetraining);
-        saveTraining.setOnClickListener(new View.OnClickListener() {
+        search = rootView.findViewById(R.id.search);
+        search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                searchForDate();
+
+            }
+        });
+        dayView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance(TimeZone.getDefault()); // Get current date
+
+// Create the DatePickerDialog instance
+                DatePickerDialog datePicker = new DatePickerDialog(getContext(),
+                        R.style.Theme_AppCompat_Light, datePickerListener,
+                        cal.get(Calendar.YEAR),
+                        cal.get(Calendar.MONTH),
+                        cal.get(Calendar.DAY_OF_MONTH));
+                datePicker.setCancelable(false);
+                datePicker.setTitle("Select the date");
+                datePicker.show();
+
+// Listener
+
+            }
+        });
+        monthView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance(TimeZone.getDefault()); // Get current date
+
+// Create the DatePickerDialog instance
+                DatePickerDialog datePicker = new DatePickerDialog(getContext(),
+                        R.style.Theme_AppCompat_Light, datePickerListener,
+                        cal.get(Calendar.YEAR),
+                        cal.get(Calendar.MONTH),
+                        cal.get(Calendar.DAY_OF_MONTH));
+                datePicker.setCancelable(false);
+                datePicker.setTitle("Select the date");
+                datePicker.show();
+
+// Listener
 
             }
         });
 
         key = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        calendar = rootView.findViewById(R.id.calendarView);
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month,
-                                            int dayOfMonth) {
-                d = dayOfMonth;
-                m = month + 1;
-                selectedDate = String.valueOf(d)+ "_" + String.valueOf(m) + "_" + String.valueOf(year);
-            }
-        });
 
 
         saveTraining.setOnClickListener(new View.OnClickListener() {
@@ -126,7 +163,7 @@ public class Tab2Calender extends Fragment {
 
 
     public void saveTraining(){
-        if (selectedDate != null) {
+        if (!dayView.getText().toString().equals("")) {
             if (!time.getText().toString().equals("")) {
                 databaseReferencecalender = FirebaseDatabase.getInstance().getReference("TrainingsDate");
                 trainingType = training.getSelectedItemPosition();
@@ -158,6 +195,32 @@ public class Tab2Calender extends Fragment {
 
         // when dialog box is closed, below method will be called.
 
+    };
+
+    private void searchForDate() {
+        if (!dayView.getText().toString().equals("")) {
+                Intent intent = new Intent(getActivity(), DateResults.class);
+                intent.putExtra("day", String.valueOf(d));
+                intent.putExtra("month", Converter.monthConverter(m));
+                startActivity(intent);
+
+
+        }else{
+            Toast.makeText(getActivity(), "Please Select a Date", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+
+        // when dialog box is closed, below method will be called.
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+            m =selectedMonth + 1;
+            d = selectedDay ;
+            dayView.setText(String.valueOf(d));
+            monthView.setText(String.valueOf(m));
+
+        }
     };
 
 }
