a4d369b41f43e8bedd1f53278f7564dd771da828
Alex V <a.vahid@campus.lmu.de>
2018-02-01 18:35:43 +0100

    fix

--- app/src/main/java/com/petarzoric/fitogether/SearchResults.java
+++ app/src/main/java/com/petarzoric/fitogether/SearchResults.java
@@ -25,8 +25,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
-
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.HashMap;
 
